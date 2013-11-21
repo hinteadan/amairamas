03e0b86fffa25f47585744b88c43506b4d7ff628
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-11-21 22:13:26 +0200

    End count when event expires

--- Source/js/script.js
+++ Source/js/script.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var counterJq = $('.counter'),
-        endDate = moment('23-11-2013 12:30:00', 'DD-MM-YYYY HH:mm:ss'),
+        endDate = moment("23-11-2013 12:30:00", "DD-MM-YYYY HH:mm:ss"),
         resolution = 100;
 
     function pad(v, desiredLength) {
@@ -15,17 +15,25 @@
     }
 
     function updateCounter() {
+
+        if (moment().isAfter(endDate) || moment().isSame(endDate)) {
+            counterJq.html('Event already happened on ' + endDate.format('MMMM Do, YYYY') + ' at ' + endDate.format('HH:mm:ss'));
+            return false;
+        }
+
         var now = moment(),
             hours = pad(endDate.diff(now, 'hours')),
             minutes = pad(endDate.diff(now, 'minutes') - 60 * hours),
             seconds = pad(endDate.diff(now, 'seconds') - hours * 60 * 60 - 60 * minutes),
             mseconds = pad(endDate.diff(now, 'milliseconds') - hours * 60 * 60 * 1000 - minutes * 60 * 1000 - seconds * 1000, 3);
         counterJq.html(hours + 'h ' + minutes + 'm ' + seconds + '.' + mseconds + 's');
+        return true;
     }
 
     function updateCounterAndQueueAnother() {
-        updateCounter();
-        setTimeout(updateCounterAndQueueAnother, resolution);
+        if (updateCounter()) {
+            setTimeout(updateCounterAndQueueAnother, resolution);
+        }
     }
 
     updateCounterAndQueueAnother();
