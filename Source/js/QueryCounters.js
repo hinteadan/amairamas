1c3faf4e38f6beb4b7105db07c0c37f7fe72075c
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2013-12-04 11:57:43 +0200

    Define API

--- Source/js/QueryCounters.js
+++ Source/js/QueryCounters.js
@@ -1,6 +1,22 @@
 ﻿(function (undefined) {
     'use strict';
 
+    //function QueryCounters() {
 
+    //}
+
+    this.Counter = this.Counter || {};
+    this.Counter.findAll = function () {
+        
+    };
+
+    /*
+    Counter.find().byId()
+    Counter.findAll()
+        .where('name')(ds.is.EqualTo)('danish')
+        .where('name')(ds.is.EqualTo)('danish')
+        .then(function(result){
+        });
+    */
 
 }).call(this);
