3004f0b255ce71d2ead4230f78ec73777394f4d5
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-12-07 17:43:40 +0200

    Create binded view/model

--- Source/js/CreateCounter/ViewModel.js
+++ Source/js/CreateCounter/ViewModel.js
@@ -1,11 +1,36 @@
-﻿(function (undefined) {
+﻿(function (ko, moment, _, undefined) {
     'use strict';
 
-    function ViewModel() {
-
+    function Month(name, index) {
+        this.name = name;
+        this.index = index;
     }
 
-    this.CreateCounter = this.CreateCounter || {};
-    this.CreateCounter.ViewModel = ViewModel;
+    function fetchMonths(){
+        var m = moment();
+        return _(_.range(0, 12)).map(function(i){
+            return new Month(m.month(i).format('MMM'), i);
+        }).value();
+    }
 
-}).call(this);
+    function ViewModel() {
+        var now = moment(),
+            yearsToShow = 10,
+            months = fetchMonths();
+
+        this.year = ko.observable(now.year());
+        this.month = ko.observable(months[now.month()]);
+        this.day = ko.observable(now.date());
+        this.hour = ko.observable(now.hour());
+        this.minute = ko.observable(now.minute());
+        this.second = ko.observable(now.second());
+        this.title = ko.observable('');
+
+        this.years = ko.observableArray(_.range(this.year(), this.year() + yearsToShow));
+        this.months = ko.observableArray(months);
+        this.days = ko.observableArray(_.range(1, 31));
+    }
+
+    ko.applyBindings(new ViewModel());
+
+}).call(this, this.ko, this.moment, this._);
