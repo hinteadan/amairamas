891fc0b918e3e7a214aca7621a4a2ec5b1d8786d
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2013-12-19 13:57:59 +0200

    Refactor weeks generation process

--- Source/js/Calendar/ViewModel.js
+++ Source/js/Calendar/ViewModel.js
@@ -21,12 +21,25 @@
                 { Label: 'Noi', Index: 10 },
                 { Label: 'Dec', Index: 11 }
             ],
+            weeks = ko.observableArray([]),
             selectedDate = ko.observable(null);
 
         function initializeYears() {
             for (var y = year() ; y < year() + yearsToShow; y++) {
                 years.push(y);
             }
+        }
+
+        function initializeWeeks() {
+            weeks.removeAll();
+            weeks([
+            [25, 26, 27, 28, 29, 30, 1],
+            [2, 3, 4, 5, 6, 7, 8],
+            [9, 10, 11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20, 21, 22],
+            [23, 24, 25, 26, 27, 28, 29],
+            [30, 31, 1, 2, 3, 4, 5]
+            ]);
         }
 
         function calculateDayLevel(day) {
@@ -51,6 +64,7 @@
         }
 
         initializeYears();
+        initializeWeeks();
 
         this.today = today;
         this.dayClass = generateDayCssClass;
@@ -59,14 +73,7 @@
         this.years = years;
         this.year = year;
         this.month = month;
-        this.weeks = [
-            [25, 26, 27, 28, 29, 30, 1],
-            [2, 3, 4, 5, 6, 7, 8],
-            [9, 10, 11, 12, 13, 14, 15],
-            [16, 17, 18, 19, 20, 21, 22],
-            [23, 24, 25, 26, 27, 28, 29],
-            [30, 31, 1, 2, 3, 4, 5]
-        ];
+        this.weeks = weeks;
         this.setDate = setSelectedDate;
         this.selectedDate = selectedDate;
     }
