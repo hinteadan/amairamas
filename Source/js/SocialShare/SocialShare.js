62d91491a6f1da09f0f9f04f0e766e2d8e9fa3df
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-12-30 20:29:20 +0200

    Twitter Share

--- Source/js/SocialShare/SocialShare.js
+++ Source/js/SocialShare/SocialShare.js
@@ -1,1 +1,26 @@
-﻿
+﻿(function ($, undefined) {
+    'use strict';
+
+    var protocol = /^http:/.test(this.window.location.href) ? 'http' : 'https';
+
+    function SocialShare() {
+
+        var self = this;
+
+        function loadTwitterWidgetApi() {
+            if ($('#twitter-wjs').length) {
+                return;
+            }
+            $('head')
+                .append('<script id="twitter-wjs" src="' + protocol + '://platform.twitter.com/widgets.js"></script>');
+
+            return self;
+        }
+
+        this.includeTwitter = loadTwitterWidgetApi;
+    }
+
+    new SocialShare()
+        .includeTwitter();
+
+}).call(this, this.$);
