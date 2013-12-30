e7771c0770264f39169bed9bfce07a2993e2ad5f
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-12-30 21:02:55 +0200

    Add Facebook share button

--- Source/js/SocialShare/SocialShare.js
+++ Source/js/SocialShare/SocialShare.js
@@ -8,6 +8,7 @@
         var self = this;
 
         function loadTwitterWidgetApi() {
+            ///<return type='SocialShare' />
             if ($('#twitter-wjs').length) {
                 return;
             }
@@ -17,10 +18,24 @@
             return self;
         }
 
+        function loadFacebookShareButtonApi() {
+            ///<return type='SocialShare' />
+            if ($('#facebook-jssdk').length) {
+                return;
+            }
+            $('head')
+                .append('<div id="fb-root"></div>')
+                .append('<script id="facebook-jssdk" src="' + protocol + '://connect.facebook.net/en_US/all.js#xfbml=1"></script>');
+
+            return self;
+        }
+
         this.includeTwitter = loadTwitterWidgetApi;
+        this.includeFacebookShare = loadFacebookShareButtonApi;
     }
 
     new SocialShare()
-        .includeTwitter();
+        .includeTwitter()
+        .includeFacebookShare();
 
 }).call(this, this.$);
