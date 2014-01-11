2dfcb89e775ae350e4819a4a40e7ee1488e0aaa3
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-01-11 23:48:49 +0200

    Tweak Frontend proxy handler and integrate it with the Http Data Store

--- Source/FrontendProxy/EventDetailsProvider.cs
+++ Source/FrontendProxy/EventDetailsProvider.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Configuration;
 using H.Crawly.Common;
+using HttpDataStore.Client;
 
 namespace FrontendProxy
 {
     public class EventDetailsProvider : IPopulateCrawlyTemplate
     {
+        private Guid eventId;
+
         public IPopulateCrawlyTemplate For(NameValueCollection queryString)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(queryString["e"]))
+            {
+                throw new InvalidOperationException("Event Id not sent");
+            }
+            this.eventId = Guid.Parse(queryString["e"]);
+            return this;
         }
 
         public Dictionary<string, string> WithThese()
         {
-            throw new NotImplementedException();
+            var eventInfo = new Store<EventInfo>(
+                ConfigurationManager.AppSettings["DataStore.DbName"],
+                ConfigurationManager.AppSettings["DataStore.Url"])
+                .Load(eventId)
+                .Data;
+            return new Dictionary<string, string> { 
+                { "title", eventInfo.title },
+                { "description", eventInfo.description },
+                { "wallUrl", eventInfo.wallUrl },
+                { "endsOn", eventInfo.endsOn.ToString() }
+            };
         }
+    }
+
+    public class EventInfo
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string wallUrl { get; set; }
+        public DateTime endsOn { get; set; }
     }
 }
