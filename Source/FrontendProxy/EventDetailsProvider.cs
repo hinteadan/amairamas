21091a95adaf28a1afdef2945267b5ae56dcb11c
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-01-11 23:00:01 +0200

    Scaffold EventDataSource provider for the frontend proxy

---
+++ Source/FrontendProxy/EventDetailsProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using H.Crawly.Common;
+
+namespace FrontendProxy
+{
+    public class EventDetailsProvider : IPopulateCrawlyTemplate
+    {
+        public IPopulateCrawlyTemplate For(NameValueCollection queryString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> WithThese()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
