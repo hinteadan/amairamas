4f8c95e7a40e1505e7b59b9ac33d7ad9a03669ee
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2015-05-04 02:25:22 +0300

    Refactor formatTime

--- SourceV2/test/time.js
+++ SourceV2/test/time.js
@@ -5,20 +5,32 @@
     var timestampNow = 0;
 
     function formatTime(timestamp) {
-        var now = new Date().getTime();
-        return { time: Math.floor(Math.abs((timestamp - now) / 1000)), unit: 's', past: timestamp <= now };
+        var now = new Date().getTime(),
+            diff = Math.floor(Math.abs((timestamp - now) / 1000));
+
+
+
+        return { time: diff, unit: 's', past: timestamp <= now };
     }
 
     describe('Time formatter', function () {
 
-        it('displays number of seconds when span is 60 seconds or less', function () {
-            expect(formatTime(new Date().getTime() + 60 * 1000)).toEqual({ time: 60, unit: 's', past: false });
+        it('displays number of seconds when span is 59 seconds or less', function () {
+            expect(formatTime(new Date().getTime() + 59 * 1000)).toEqual({ time: 59, unit: 's', past: false });
             expect(formatTime(new Date().getTime() + 10 * 1000)).toEqual({ time: 10, unit: 's', past: false });
 
             expect(formatTime(new Date().getTime())).toEqual({ time: 0, unit: 's', past: true });
 
             expect(formatTime(new Date().getTime() - 10 * 1000)).toEqual({ time: 10, unit: 's', past: true });
-            expect(formatTime(new Date().getTime() - 60 * 1000)).toEqual({ time: 60, unit: 's', past: true });
+            expect(formatTime(new Date().getTime() - 59 * 1000)).toEqual({ time: 59, unit: 's', past: true });
+        });
+
+        xit('displays number of minutes when span is 1 to 59 minutes', function () {
+            expect(formatTime(new Date().getTime() + 60 * 1000)).toEqual({ time: 1, unit: 'm', past: false });
+            expect(formatTime(new Date().getTime() - 60 * 1000)).toEqual({ time: 1, unit: 'm', past: true });
+
+            expect(formatTime(new Date().getTime() + 59 * 60 * 1000)).toEqual({ time: 59, unit: 'm', past: false });
+            expect(formatTime(new Date().getTime() - 59 * 60 * 1000)).toEqual({ time: 59, unit: 'm', past: true });
         });
 
     });
