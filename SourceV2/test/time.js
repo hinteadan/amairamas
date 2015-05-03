6a779eb409a1a6ed8d2bbaaba22704e731221f0a
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2015-05-04 02:38:42 +0300

    Refactor formatTime

--- SourceV2/test/time.js
+++ SourceV2/test/time.js
@@ -5,12 +5,21 @@
     var timestampNow = 0;
 
     function formatTime(timestamp) {
-        var now = new Date().getTime(),
-            diff = Math.floor(Math.abs((timestamp - now) / 1000));
+        var units = ['s', 'm', 'h', 'd', 'M', 'y'],
+            unitBases = [1000, 60, 60, 24, 30, 12],
+            now = new Date().getTime(),
+            diff = Math.abs(timestamp - now),
+            unit = units[0];
 
+        for (var i = 0; i < units.length; i++) {
+            if(Math.floor(diff / unitBases[i]) === 0){
+                break;
+            }
+            diff = Math.floor(diff / unitBases[i]);
+            unit = units[i];
+        }
 
-
-        return { time: diff, unit: 's', past: timestamp <= now };
+        return { time: diff, unit: unit, past: timestamp <= now };
     }
 
     describe('Time formatter', function () {
@@ -25,7 +34,7 @@
             expect(formatTime(new Date().getTime() - 59 * 1000)).toEqual({ time: 59, unit: 's', past: true });
         });
 
-        xit('displays number of minutes when span is 1 to 59 minutes', function () {
+        it('displays number of minutes when span is 1 to 59 minutes', function () {
             expect(formatTime(new Date().getTime() + 60 * 1000)).toEqual({ time: 1, unit: 'm', past: false });
             expect(formatTime(new Date().getTime() - 60 * 1000)).toEqual({ time: 1, unit: 'm', past: true });
 
