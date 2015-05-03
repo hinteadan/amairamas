0e97a31e79d1fa3a33b3b2ef451db658bcadae1b
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2015-05-04 01:04:37 +0300

    OD: Unit Test Time formatter

--- SourceV2/test/time.js
+++ SourceV2/test/time.js
@@ -2,10 +2,25 @@
 
     'use strict';
 
-    describe('A Dummy Test', function () {
-        it('should assert something', function () {
-            expect(true).toBe(true);
+    var timestampNow = 0;
+
+    function formatTime(timestamp) {
+        var now = new Date().getTime();
+        return { time: Math.floor(Math.abs((timestamp - now) / 1000)), unit: 's', past: timestamp <= now };
+    }
+
+    describe('Time formatter', function () {
+
+        it('displays number of seconds when span is 60 seconds or less', function () {
+            expect(formatTime(new Date().getTime() + 60 * 1000)).toEqual({ time: 60, unit: 's', past: false });
+            expect(formatTime(new Date().getTime() + 10 * 1000)).toEqual({ time: 10, unit: 's', past: false });
+
+            expect(formatTime(new Date().getTime())).toEqual({ time: 0, unit: 's', past: true });
+
+            expect(formatTime(new Date().getTime() - 10 * 1000)).toEqual({ time: 10, unit: 's', past: true });
+            expect(formatTime(new Date().getTime() - 60 * 1000)).toEqual({ time: 60, unit: 's', past: true });
         });
+
     });
 
 })();
