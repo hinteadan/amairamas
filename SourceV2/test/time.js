d0accc960e6b1628a92cc54d84dac3edcb1e377b
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-05-04 15:24:58 +0300

    Complete Unit Tests for time formatter

--- SourceV2/test/time.js
+++ SourceV2/test/time.js
@@ -50,6 +50,30 @@
             expect(formatTime(new Date().getTime() - 23 * 60 * 60 * 1000)).toEqual({ time: 23, unit: 'h', past: true });
         });
 
+        it('displays number of days when span is 1 to 29 days', function () {
+            expect(formatTime(new Date().getTime() + 24 * 60 * 60 * 1000)).toEqual({ time: 1, unit: 'd', past: false });
+            expect(formatTime(new Date().getTime() - 24 * 60 * 60 * 1000)).toEqual({ time: 1, unit: 'd', past: true });
+
+            expect(formatTime(new Date().getTime() + 29 * 24 * 60 * 60 * 1000)).toEqual({ time: 29, unit: 'd', past: false });
+            expect(formatTime(new Date().getTime() - 29 * 24 * 60 * 60 * 1000)).toEqual({ time: 29, unit: 'd', past: true });
+        });
+
+        it('displays number of months when span is 30 days to 11 months', function () {
+            expect(formatTime(new Date().getTime() + 30 * 24 * 60 * 60 * 1000)).toEqual({ time: 1, unit: 'M', past: false });
+            expect(formatTime(new Date().getTime() - 30 * 24 * 60 * 60 * 1000)).toEqual({ time: 1, unit: 'M', past: true });
+
+            expect(formatTime(new Date().getTime() + 11 * 30 * 24 * 60 * 60 * 1000)).toEqual({ time: 11, unit: 'M', past: false });
+            expect(formatTime(new Date().getTime() - 11 * 30 * 24 * 60 * 60 * 1000)).toEqual({ time: 11, unit: 'M', past: true });
+        });
+
+        it('displays number of years when span is 12 months or more', function () {
+            expect(formatTime(new Date().getTime() + 12 * 30 * 24 * 60 * 60 * 1000)).toEqual({ time: 1, unit: 'y', past: false });
+            expect(formatTime(new Date().getTime() - 12 * 30 * 24 * 60 * 60 * 1000)).toEqual({ time: 1, unit: 'y', past: true });
+
+            expect(formatTime(new Date().getTime() + 166 * 12 * 30 * 24 * 60 * 60 * 1000)).toEqual({ time: 166, unit: 'y', past: false });
+            expect(formatTime(new Date().getTime() - 111 * 12 * 30 * 24 * 60 * 60 * 1000)).toEqual({ time: 111, unit: 'y', past: true });
+        });
+
     });
 
 })();
