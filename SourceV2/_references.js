56c4af7bfa74e64c404c84cd16ebca17c4f887f9
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-05-05 12:59:03 +0300

    Scaffold TimelineJS

--- SourceV2/_references.js
+++ SourceV2/_references.js
@@ -1,8 +1,12 @@
-﻿/// <reference path="~/app/lib/jasmine/lib/jasmine-core/jasmine.js" />
+﻿/// <reference path="~/app/lib/jquery/dist/jquery.js" />
+/// <reference path="~/app/lib/jasmine/lib/jasmine-core/jasmine.js" />
 /// <reference path="~/app/lib/jasmine/lib/jasmine-core/boot.js" />
 /// <reference path="~/app/js/timeFormatter.js" />
 /// <reference path="~/app/lib/angular/angular.js" />
 /// <reference path="~/app/lib/angular-route/angular-route.js" />
+/// <reference path="~/app/lib/TimelineJS/build/js/timeline.js" />
+/// <reference path="~/app/lib/TimelineJS/build/js/storyjs-embed.js" />
+
 
 /// <reference path="~/app/js/repository/module.js" />
 /// <reference path="~/app/js/repository/dummy.js" />
---
+++ SourceV2/app/js/timeline/timeline-drtv.js
@@ -0,0 +1,30 @@
+﻿(function (ng, createTimeline) {
+    'use strict';
+
+    ng.module('timeline').directive('timeline', [function () {
+
+        return {
+            restrict: 'A',
+            replace: false,
+            transclude: false,
+            scope: {
+                config: '=',
+                source: '='
+            },
+            link: function ($s, $e, $a) {
+                /*jshint unused: false*/
+                var id = 'timeline_' + new Date().getTime();
+                $e.attr('id', id);
+                createTimeline({
+                    type: 'timeline',
+                    width: '100%',
+                    height: '100%',
+                    source: $s.source,
+                    embed_id: id
+                });
+            }
+        };
+
+    }]);
+
+})(this.angular, this.createStoryJS);
