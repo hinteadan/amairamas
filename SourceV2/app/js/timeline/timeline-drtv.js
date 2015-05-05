f1ea323b80c016909865e46d84408a842abf3f65
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-05-05 15:29:43 +0300

    Sample to handle TimelineJS events

--- SourceV2/app/js/timeline/timeline-drtv.js
+++ SourceV2/app/js/timeline/timeline-drtv.js
@@ -3,18 +3,34 @@
 
     ng.module('timeline').directive('timeline', [function () {
 
+        /*jshint unused: false*/
+        var timelineEvents = {
+            loaded: 'LOADED',
+            update: 'UPDATE',
+            message: 'MESSAGE',
+            messege: 'MESSEGE',
+            dataReady: 'DATAREADY',
+            headline: 'HEADLINE',
+            slideChange: 'SLIDE_CHANGE',
+            dragUpdate: 'DRAGUPDATE'
+        };
+
         return {
             restrict: 'A',
             replace: false,
             transclude: false,
             scope: {
-                config: '=',
                 source: '=timeline'
             },
             link: function ($s, $e, $a) {
                 /*jshint unused: false*/
                 var id = 'timeline_' + new Date().getTime();
                 $e.attr('id', id);
+
+                //VMM.bindEvent($e, function () {
+                //    console.log('UPDATE');
+                //    console.log(arguments);
+                //}, 'UPDATE');
 
                 createTimeline({
                     type: 'timeline',
